07e5a9607afaf6e3486e636c6078c7e0b387bb04
Peter Ndungu <93506710+petreleven@users.noreply.github.com>
2025-11-20 10:53:11 +0300

    Fix quit event handling in space.py

--- space.py
+++ space.py
@@ -6,11 +6,19 @@ height = 400
 
 gamescreen =pygame.display.set_mode((width,height))
 
+#ship details
+ship_positionx =  300
+ship_rectangle = pygame.Rect(300, 380, width=50, height=50)
+
+
+
+
 #forever block  using(while loop)
 while True:
     buttons = pygame.event.get()
     for button in buttons:
-        if button.type == pygame.KEYDOWN:
+        if button.key == pygame.QUIT: #is that button the quit          #new
+            exit()                    #exit the game                    #new
 
 
 
